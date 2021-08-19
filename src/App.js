ede3b04136c6b765fe1e504b6d91dd5575b3792a
Daniel Caballero <danielditer@gmail.com>
2021-08-19 18:58:45 -0400

    Practica 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+import Product from "./screens/Product";
+import Default from "./screens/Default";
+
+function App() {
+  return (
+    <div className="App">
+        <Default />
+        <Product />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/Product.jsx
@@ -0,0 +1,18 @@
+import FilterableProductTable from "./Product/_components/FilterableProductTable";
+
+const Product = () => {
+    return <div className="Product">
+        <FilterableProductTable products={PRODUCTS}/>
+    </div>
+}
+
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default Product;
